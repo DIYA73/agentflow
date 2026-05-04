f3d160305b16234d20f2309f2390fd2eb62ba83b
didi <diyaismahil7@gmail.com>
2026-05-05 00:04:43 +0300

    chore: remove phase scaffolds from repo

--- agentflow-phase3/apps/web/src/app/flows/layout.tsx
+++
@@ -1,4 +0,0 @@
-import { AppShell } from '@/components/layout/AppShell';
-export default function FlowsLayout({ children }: { children: React.ReactNode }) {
-  return <AppShell>{children}</AppShell>;
-}
