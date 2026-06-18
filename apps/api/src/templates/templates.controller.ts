fdd6670ba855ea3f8947de1705108c01983ddf65
didi <diyaismahil7@gmail.com>
2026-06-19 01:31:31 +0300

    fix: mark GET /templates and GET /templates/:id as public

--- apps/api/src/templates/templates.controller.ts
+++ apps/api/src/templates/templates.controller.ts
@@ -3,6 +3,7 @@ import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { TemplatesService } from './templates.service';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 import { CurrentWorkspaceId } from '../common/decorators/current-user.decorator';
+import { Public } from '../common/decorators/public.decorator';
 import type { TemplateCategory } from './template.entity';
 
 @ApiTags('Templates')
@@ -10,11 +11,13 @@ import type { TemplateCategory } from './template.entity';
 export class TemplatesController {
   constructor(private readonly service: TemplatesService) {}
 
+  @Public()
   @Get()
   findAll(@Query('category') category?: TemplateCategory) {
     return this.service.findAll(category);
   }
 
+  @Public()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.service.findOne(id);
